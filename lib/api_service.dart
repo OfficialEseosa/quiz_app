07830e7900cf8f2d430d7f6ff20d3daed21fbecd
Raphael Omorose <OfficialEseosa@users.noreply.github.com>
2026-04-16 10:52:00 -0400

    Implement fetchQuestions: GET request and JSON parsing

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -6,4 +6,19 @@ class ApiService {
   static const String _baseUrl = 'https://opentdb.com/api.php';
   static const String _queryParams =
       '?amount=10&category=9&difficulty=easy&type=multiple';
+
+  Future<List<Question>> fetchQuestions() async {
+    final uri = Uri.parse('$_baseUrl$_queryParams');
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body) as Map<String, dynamic>;
+      final results = data['results'] as List<dynamic>;
+      return results
+          .map((item) => Question.fromJson(item as Map<String, dynamic>))
+          .toList();
+    } else {
+      throw Exception('Server error: ${response.statusCode}');
+    }
+  }
 }
